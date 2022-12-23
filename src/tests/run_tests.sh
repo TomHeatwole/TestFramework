7e7c344675ac143fab602c09cc54c0cfc3e6b358
tomheatwole <tgh8xn@virginia.edu>
2022-12-23 10:14:46 -0500

    Better accountability in meta test script

--- src/tests/run_tests.sh
+++ src/tests/run_tests.sh
@@ -6,33 +6,44 @@ DuplicateNameTest
 "
 
 declare -i total=0
-declare -i passed=0
+declare -i pass=0
+declare -i fail=0
+declare -i bad_compile=0
 
 for f in $TEST_NAMES
 do
+    echo "Processing $f.cpp..."
+    total=$((total + 1))
     g++ $f.cpp -std=c++17
     if [ $? -eq 0 ]; then
-        total=$((total + 1))
-        echo "Processing $f.cpp..."
 
         OUTPUT=$(./a.out)
         EXPECTED_OUTPUT=$(cat ${f}_EXPECTED.txt)
 
         if [ "$OUTPUT" = "$EXPECTED_OUTPUT" ]; then
             echo "...OK"
-            passed=$((passed + 1))
+            pass=$((pass + 1))
         else
+            fail=$((fail + 1))
             echo "Unexpected output:"
             echo ${OUTPUT}
             echo 
         fi
+    else
+        bad_compile=$((bad_compile + 1))
     fi
 done
 
 
 echo 
-if [ $passed -eq $total ]; then
-    echo "All tests passed successfully!"
+if [ $pass -eq $total ]; then
+    echo "All tests pass successfully!"
 else
-    echo "$passed of $total total tests passed."
+    echo "$pass of $total total tests pass."
+    if [ $fail -ne 0 ]; then
+        echo "    $fail tests produced the wrong output."
+    fi
+    if [ $bad_compile -ne 0 ]; then
+        echo "    $bad_compile tests failed to compile."
+    fi
 fi
