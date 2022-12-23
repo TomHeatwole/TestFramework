f4ea7a1b2842b38b2e815d2d51ee5f555a0a26b5
tomheatwole <tgh8xn@virginia.edu>
2022-12-23 11:56:09 -0500

    Remove std::cout from example tests

--- src/tests/BasicTest.cpp
+++ src/tests/BasicTest.cpp
@@ -3,15 +3,12 @@
 TEST_FILE
 
 TEST("ExampleTest1") {
-    std::cout << "example 1" << std::endl;
 }
 
 TEST("ExampleTest2") {
-    std::cout << "example 2" << std::endl;
 }
 
 TEST("ExampleTest3") {
-    std::cout << "example 3" << std::endl;
 }
 
 
